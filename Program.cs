81bbad13a316fc4ba2a38b1846e8f021891b2726
Bolseva <bolsevasona@gmail.com>
2024-10-16 12:42:56 +0300

    added Hello

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace BolsevaGit1
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            int b = 5;
+            Console.WriteLine(a + b);
+
         }
     }
 }
